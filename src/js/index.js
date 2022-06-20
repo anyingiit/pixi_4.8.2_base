094b5d3ba8465aec61109d7ed452199268b9c481
anyingiit <anyingiit@gmail.com>
2022-06-20 22:39:50 +0800

    基于webpack的pixi基础项目搭建完毕 关键依赖:     "@types/pixi.js": "4.8.2",     "pixi.js": "4.8.2",     "webpack": "^5.73.0"

---
+++ src/js/index.js
@@ -0,0 +1,36 @@
+import * as PIXI from 'pixi.js';
+
+const app = new PIXI.Application(510, 674)
+document.body.appendChild(app.view)
+
+const bg = new PIXI.Sprite.fromImage('assets/bg3.jpg')
+app.stage.addChild(bg)
+
+
+const plane = new PIXI.Sprite.fromImage('assets/plane.png')
+plane.x = 195
+plane.y = 540
+app.stage.addChild(plane)
+
+
+const hp = new PIXI.Sprite.fromImage('assets/hp.png')
+hp.x = 5
+hp.y = 10
+app.stage.addChild(hp)
+
+
+const score = new PIXI.Text("得分: 10000")
+score.x = 320;
+score.y = 10;
+score.style.fill = "white"
+app.stage.addChild(score)
+
+
+const cloud = new PIXI.Sprite.fromImage('assets/yun02.png')
+cloud.y = 100
+app.stage.addChild(cloud)
+
+const exp = new PIXI.Sprite.fromImage('assets/exp.png')
+exp.x = 100
+exp.y = 300
+app.stage.addChild(exp)
