5d76747798ada22df5b57424d4190848178b8fe7
anyingiit <49945850+anyingiit@users.noreply.github.com>
2022-06-21 11:00:01 +0800

    add ts support 1. 现在可以使用ts文件了 2. ts文件使用ES标准, 并且最高支持到ES2020

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ const webpack = require('webpack')
 
 
 module.exports = {
-    entry: path.resolve(__dirname, 'src/js/index.js'),// webpack打包入口, webpack会根据该文件的依赖生成依赖树
+    entry: path.resolve(__dirname, 'src/js/index.ts'),// webpack打包入口, webpack会根据该文件的依赖生成依赖树
     output: {
         path: path.resolve(__dirname, 'dist'),// 输出位置
         filename: 'bundle.[hash:8].js'// 输出文件名称
@@ -22,7 +22,14 @@ module.exports = {
     target: 'web',// 输出模式: web
     module: {
         rules: [
-            {// 允许webpack持续维护source map数据, 使其更易于调试
+            {
+                // 从 JavaScript 入口提取现有的 source maps. 这些 source maps 既可以是内联的也可以是通过 URL 链接引入的。
+                // 所有的 source map 数据都按照选定的 source map style 交给 webpack 处理，这些选定可以在 webpack.config.js 的 devtool 选项中配置。
+                // 在使用有自己 source maps 的第三方库时，source-map-loader 就显得尤为重要。
+                // 如果相关 source map 数据没有按照规范提取、处理并注入 webpack bundle, 浏览器有可能无法正确解读这些数据。
+                // source-map-loader 允许 webpack 跨库且持续的维护 source map 数据，因而更易于调试。 source-map-loader 可以从任何 JavaScript 文件中提取，这也包括 node_modules 目录下的 JavaScript 文件。 在设置 include 和 exclude 规则时，要保证构建性能最优。
+
+                // 解决的问题就是在node_modules中某些库拥有source map文件, 但是webpack默认在构建时不会将这些source map文件放入到bundle.js中, 而当这些库有source map并使用他们时, 就会找不到, 所以我们使用这个source-map-loader将所有(如果存在)的source map文件放入到bundle.js中, 那么在这些库加载source map时就不会有问题了
                 test: /\.js$/,
                 // loader的书写顺序很重要，因为loader的职责单一，组合使用的原则，所有loader一个一个处理的顺序很重要
                 // 怎么调整这个顺序很关键，但是如果你书写的时候已经排好了序，那就无所谓了
@@ -34,7 +41,15 @@ module.exports = {
                 enforce: "pre",
                 use: ["source-map-loader"],
             },
+            {
+                test: /\.ts$/,
+                use: ["ts-loader"],
+                exclude: /node_modules/,
+            }
         ],
+    },
+    resolve: {
+        extensions: ['.js', '.ts']
     },
     plugins: [// 配置webpack插件
         new CleanWebpackPlugin(),// 配置清除插件, 改插件可以在每次打包时清除输出目录内的所有上次生成的文件, 确保输出文件夹清洁
